3f999d841d8ff92ad48777b2de9a722854b2a957
Dave <davidalejandrodand@gmail.com>
2021-12-07 07:57:37 -0400

    Añadido nueva url para procesar las solicitudes.

--- enterprises/empresa/urls.py
+++ enterprises/empresa/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
 
-from .views import IndexView, TablaEmpresaView, EmpresaDetailView
+from .views import (
+   IndexView, TablaEmpresaView, 
+   EmpresaDetailView, RequestEmpresaInfoView
+)
 
 
 urlpatterns = [
    path('', IndexView.as_view(), name='index'),
    path('tabla-empresas/', TablaEmpresaView.as_view(), name='tabla_empresas'),
-   path('empresa-detail/<int:pk>/', EmpresaDetailView.as_view(), name='empresa_detail')
+   path('empresa-detail/<int:pk>/', EmpresaDetailView.as_view(), name='empresa_detail'),
+   path('tabla-empresas/request-empresa-info/', RequestEmpresaInfoView.as_view(), name='request_empresa_info'),
 ]
